c0f343f7e8fc50fcbce458b0b5ff88a33bda9aaf
barisozluk93 <136732760+barisozluk93@users.noreply.github.com>
2025-12-22 15:50:26 +0300

    Actions fixed

--- source-expo/app/actions/index.js
+++ source-expo/app/actions/index.js
@@ -1,4 +1,6 @@
 import * as AuthActions from './auth';
 import * as ApplicationActions from './application';
+import * as UserActions from './user';
+import * as DeclarationActions from './declaration';
 
-export { AuthActions, ApplicationActions };
+export { AuthActions, ApplicationActions, UserActions, DeclarationActions };
