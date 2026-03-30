18676fb8de2a30cb3b8544cd2e2fe4fc3883818b
barisozluk93 <136732760+barisozluk93@users.noreply.github.com>
2026-03-30 16:12:48 +0300

    logo değiştirildi.

--- source-expo/app/config/setting.js
+++ source-expo/app/config/setting.js
@@ -4,7 +4,7 @@
 export const BaseSetting = {
   name: 'DCS Mobile',
   displayName: 'DCS Mobile',
-  appVersion: '0.0.4',
+  appVersion: '0.0.5',
   defaultLanguage: 'tr',
   languageSupport: [
     'en',
